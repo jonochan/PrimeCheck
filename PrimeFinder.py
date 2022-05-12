b99d1986930321c4fa996b4b2971c9028e97e480
Jonathan Chan <jonoccchan@gmail.com>
2022-05-12 08:58:06 +0100

    initial commit

---
+++ PrimeFinder.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    result= "Number is prime"
+    if n <2:
+        result = "Please enter a number greater than 2"
+    elif n%2 == 0:
+        result = "Number is not prime"
+    else:
+        fifth = n//5
+        for x in range(3, fifth):
+            if n%x == 0:
+                result = "Number is not prime"
+                return result
+    return result
+
+
